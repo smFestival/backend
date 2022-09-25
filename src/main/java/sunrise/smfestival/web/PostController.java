a3be56f0aaebbc7ebb618e0d36717870a9192d5f
la1av1a <zldqkrktqk@gmail.com>
2022-09-25 20:52:32 +0900

    !HOTFIX CORS

--- src/main/java/sunrise/smfestival/web/PostController.java
+++ src/main/java/sunrise/smfestival/web/PostController.java
@@ -14,6 +14,7 @@ import sunrise.smfestival.web.dto.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
+@CrossOrigin
 @RequiredArgsConstructor
 @RequestMapping("/post")
 @RestController
